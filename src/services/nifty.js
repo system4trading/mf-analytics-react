2b5fb3f2faec2164d77aa541d9c39e8226b2d6c8
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-16 01:48:56 +0530

    Update nifty.js

--- src/services/nifty.js
+++ src/services/nifty.js
@@ -1,4 +1,4 @@
 export const fetchNifty = () =>
-  fetch("https://raw.githubusercontent.com/YOURNAME/mf-data-cache/main/nifty/nifty50.json")
+  fetch("https://raw.githubusercontent.com/system4trading/mf-data-cache/main/nifty/nifty50.json")
     .then(r => r.json());
 
