89edff9896a6d1a7c460f21af6cd96206e6e5cac
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-16 01:33:42 +0530

    Update returns.js

--- src/analytics/returns.js
+++ src/analytics/returns.js
@@ -1,1 +1,8 @@
+export const logReturns = prices =>
+  prices.slice(1).map((p, i) => Math.log(p / prices[i]));
+
+export const mean = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+
+export const stdev = arr =>
+  Math.sqrt(mean(arr.map(x => (x - mean(arr)) ** 2)));
 
