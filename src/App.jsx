6fe34adb0aa8a80437b826633e7897fc2f80ff67
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-16 01:32:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Investor from "./routes/Investor";
+import Advisor from "./routes/Advisor";
+import Compare from "./routes/Compare";
+import Factsheet from "./routes/Factsheet";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Investor />} />
+        <Route path="/advisor" element={<Advisor />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/factsheet/:code" element={<Factsheet />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
