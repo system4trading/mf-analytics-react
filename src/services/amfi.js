1804412db0ce820f4eda1b350c1ef89ed03e9af5
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-16 01:41:20 +0530

    Update amfi.js

--- src/services/amfi.js
+++ src/services/amfi.js
@@ -1,1 +1,5 @@
+const BASE = "https://raw.githubusercontent.com/system4trading/mf-data-cache/main/amfi/";
+
+export const fetchNAV = code =>
+  fetch(`${BASE}nav_${code}.json`).then(r => r.json());
 
